3ccdd3a3c929bf78b58bb3815fb2fc14f2bd6d88
Choo Choo <sunhcc@gmail.com>
2020-08-24 17:34:32 +0800

    with js showall hideall

---
+++ dist/main.dev.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// const nyb = document.querySelectorAll('.flipButton')[0];
+// const nypop = document.querySelectorAll('div .pop')[0];
+// const lapop = c
+// const nypopup = document.querySelectorAll('.popup')[0];
+// nyb.addEventListener ('onmouseover', () => {
+//     nypop.innerHTML = 'block';
+//  });
+// nyb.addEventListener ('click', () => {
+//    nypop.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2016/11/14/03/22/elephant-1822481__480.jpg')";
+// });
+//  showface.addEventListener('onmouseout', () => {
+//     nyturn.style.transform = "rotateY(180deg)";
+//     laturn.style.transform = "rotateY(180deg)";
+//  });
+var showface = document.getElementById('title');
+var showall = document.getElementById('showall');
+var nyturn = document.querySelectorAll('.inner')[0];
+var laturn = document.querySelectorAll('.inner')[1];
+var sfturn = document.querySelectorAll('.inner')[2];
+var hnlturn = document.querySelectorAll('.inner')[3];
+var ontturn = document.querySelectorAll('.inner')[4];
+var yvrturn = document.querySelectorAll('.inner')[5];
+var rometurn = document.querySelectorAll('.inner')[6];
+var vieturn = document.querySelectorAll('.inner')[7];
+var londonturn = document.querySelectorAll('.inner')[8];
+var amsturn = document.querySelectorAll('.inner')[9];
+var fraturn = document.querySelectorAll('.inner')[10];
+var sydturn = document.querySelectorAll('.inner')[11];
+var bneturn = document.querySelectorAll('.inner')[12];
+var melturn = document.querySelectorAll('.inner')[13];
+var delturn = document.querySelectorAll('.inner')[14];
+showall.addEventListener('click', function () {
+  nyturn.style.transform = "rotateY(180deg)";
+  laturn.style.transform = "rotateY(180deg)";
+  sfturn.style.transform = "rotateY(180deg)";
+  hnlturn.style.transform = "rotateY(180deg)";
+  ontturn.style.transform = "rotateY(180deg)";
+  yvrturn.style.transform = "rotateY(180deg)";
+  rometurn.style.transform = "rotateY(180deg)";
+  vieturn.style.transform = "rotateY(180deg)";
+  londonturn.style.transform = "rotateY(180deg)";
+  amsturn.style.transform = "rotateY(180deg)";
+  fraturn.style.transform = "rotateY(180deg)";
+  sydturn.style.transform = "rotateY(180deg)";
+  bneturn.style.transform = "rotateY(180deg)";
+  melturn.style.transform = "rotateY(180deg)";
+  delturn.style.transform = "rotateY(180deg)";
+});
--- main.js
+++ main.js
@@ -1,56 +1,53 @@
-// const PhotoInfo = [
-//     '.ny',
-//     '.la',
-//     '.sf',
-//     '.hnl',
-//     '.ont',
-//     '.yvr',
-//     '.rome',
-//     '.vie',
-//     '.london',
-//     '.ams',
-//     '.fra',
-//     '.syd',
-//     '.bne',
-//     '.mel',
-//     '.del'    
-// ]
-// for(i=0,i<PhotoInfo.length, i++){
-// let e = document.querySelector('PhotoInfo[i]');
-// }
-
-// const popup = document.getElementById('popup');
-// popup.addEventListener ('onmouseover', () => {
-//  PhotoInfo.style.display = 'block';
-// });
-// const fadeout = document.getElementById('popup');
-// fadeout.addEventListener ('onmouseout', () => {
-//  PhotoInfo.style.display = 'none';
+// const nyb = document.querySelectorAll('.flipButton')[0];
+// const nypop = document.querySelectorAll('div .pop')[0];
+// const lapop = c
+// const nypopup = document.querySelectorAll('.popup')[0];
+// nyb.addEventListener ('onmouseover', () => {
+//     nypop.innerHTML = 'block';
+//  });
+// nyb.addEventListener ('click', () => {
+//    nypop.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2016/11/14/03/22/elephant-1822481__480.jpg')";
 // });
 
+//  showface.addEventListener('onmouseout', () => {
+//     nyturn.style.transform = "rotateY(180deg)";
+//     laturn.style.transform = "rotateY(180deg)";
+//  });
 
 
+const showface = document.getElementById('title');
+const showall = document.getElementById('showall');
+const nyturn = document.querySelectorAll('.inner')[0];
+const laturn = document.querySelectorAll('.inner')[1];
+const sfturn = document.querySelectorAll('.inner')[2];
+const hnlturn = document.querySelectorAll('.inner')[3];
+const ontturn = document.querySelectorAll('.inner')[4];
+const yvrturn = document.querySelectorAll('.inner')[5];
+const rometurn = document.querySelectorAll('.inner')[6];
+const vieturn = document.querySelectorAll('.inner')[7];
+const londonturn = document.querySelectorAll('.inner')[8];
+const amsturn = document.querySelectorAll('.inner')[9];
+const fraturn = document.querySelectorAll('.inner')[10];
+const sydturn = document.querySelectorAll('.inner')[11];
+const bneturn = document.querySelectorAll('.inner')[12];
+const melturn = document.querySelectorAll('.inner')[13];
+const delturn = document.querySelectorAll('.inner')[14];
+showall.addEventListener('click', () => {
+    nyturn.style.transform = "rotateY(180deg)";
+    laturn.style.transform = "rotateY(180deg)";
+    sfturn.style.transform = "rotateY(180deg)";
+    hnlturn.style.transform = "rotateY(180deg)";
+    ontturn.style.transform = "rotateY(180deg)";
+    yvrturn.style.transform = "rotateY(180deg)";
+    rometurn.style.transform = "rotateY(180deg)";
+    vieturn.style.transform = "rotateY(180deg)";
+    londonturn.style.transform = "rotateY(180deg)";
+    amsturn.style.transform = "rotateY(180deg)";
+    fraturn.style.transform = "rotateY(180deg)";
+    sydturn.style.transform = "rotateY(180deg)";
+    bneturn.style.transform = "rotateY(180deg)";
+    melturn.style.transform = "rotateY(180deg)";
+    delturn.style.transform = "rotateY(180deg)";
+ });
 
-
-// e.onmouseover = function() {
-//   document.getElementById('popup').style.display = 'block';
-// }
-// e.onmouseout = function() {
-//   document.getElementById('popup').style.display = 'none';
-// }
-
-var ee = document.querySelector('.ont');
-ee.onmouseover = function() {
-  document.getElementById('popup').style.display = 'block';
-}
-ee.onmouseout = function() {
-  document.getElementById('popup').style.display = 'none';
-}
-
-// var e = document.querySelector('.mel');
-// e.onmouseover = function() {
-//   document.getElementById('popup').style.display = 'block';
-// }
-// e.onmouseout = function() {
-//   document.getElementById('popup').style.display = 'none';
-// }
+ 
